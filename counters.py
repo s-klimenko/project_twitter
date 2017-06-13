5ad8102a06891e9a770b36cfbfa43fadf85d2012
Sveta Klimenko <veresk96@gmail.com>
2017-06-13 21:18:57 +0300

    bugs fixed

--- counters.py
+++ counters.py
@@ -1,10 +1,22 @@
-import Counter
-import Mystem
+from pymystem3 import Mystem  # импортируем майстем
+from collections import Counter
 import re
+import string
 
-def verbs_statistics(text):
+def word_counter(text):
     '''
-    Выделяет из текста данные о частотности глаголов, их лемм, вида и совершенности
+    Считаем слова
+    :param text:
+    :return:
+    '''
+    text = text.split()
+    text = text.strip(string.punctuation)
+    text = [i for i in text if i != '']
+    return(len(text))
+
+def pos_counter(text):
+    '''
+    Считаем части речи
     :param text: текст
     :return: словари с частотностью
     '''
@@ -12,30 +24,41 @@ def verbs_statistics(text):
     ana = m.analyze(text)
     pos = [i['analysis'][0]['gr'].split('=')[0].split(',')[0] for i in ana if
            i['text'].strip() and 'analysis' in i and i['analysis']]
-
-def pos_counter(pos):
-    '''
-    Считает части речи, глаголы и долю глаголов в тексте.
-    :param pos: список с частеречными тегами Mystem для всех слов в тексте
-    :type pos: list
-    :return: all_pos - количество всех частей речи (слов) в тексте
-             v - количество глаголов в тексте
-             ratio - доля глаголов в тексте
-    '''
     c_pos = Counter(pos)
     return c_pos
 
 def punct_counter(text):
-    punct = ['!', '?']
-    c_punc = re.findall('[!?]')
+    punct = re.findall('[!?]', text)
+    c_punct = (Counter(punct))
+    return c_punct
 
-def emoji_pos_counter:
+def emoji_counter(text):
+    '''
+    Считаю емодзи
+    :param emoji:
+    :return:
+    '''
+    with open ('em_neg.txt', 'r') as em_neg:
+        neg_emoji = [line.strip() for line in em_neg]
+        ne = '[' + '|'.join(neg_emoji) + ']'
+    emoji_n = re.findall(ne, text)
+    c_neg_em = {'neg_em': sum(Counter(emoji_n).values())}
+    with open('em_neg.txt', 'r') as em_pos:
+        pos_emoji = [line.strip() for line in em_pos]
+        pe = '[' + '|'.join(pos_emoji) + ']'
+    emoji_p = re.findall(pe, text)
+    c_pos_em = {'pos_em': sum(Counter(emoji_p).values())}
+    return c_neg_em, c_pos_em
 
+def smile_counter(text):
+    '''
+    Считает смайлики
+    :param smiles:
+    :return:
+    '''
+    sm_n = re.findall(r'(:\(|;\(||=\(|:-\()', text)
+    c_neg_smiles = {'neg_sm' : sum(Counter(sm_n).values())}
+    sm_p =  re.findall(r'(:\)|:D|: @|;\)|:-\)|=\)|:\*|:3)')
+    c_pos_smiles = {'pos_sm' : sum(Counter(sm_p).values())}
+    return c_neg_smiles, c_pos_smiles
 
-def emoji_neg_counter:
-
-def smile_pos_counter:
-    pos_smiles =  (':)', ':D',': @', ';)' ':-)', '=)', ':*', ':3')
-
-def smile_neg_counter:
-    neg_smiles = (':(', ';(', ': @', '=(', ':-(')
